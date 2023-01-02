dc5f3d21858548ce94d45cb94399e7da2891ded7
mertguler94 <gulermert94@gmail.com>
2023-01-02 01:08:39 -0500

    create simple header/footer layout

---
+++ src/components/CallToAction.tsx
@@ -0,0 +1,15 @@
+function CallToAction() {
+  return (
+    <section className="border w-1/2 p-10">
+      <h1 className="text-8xl">Hi, I&apos;m Mert!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        voluptatum ab ipsam et nostrum exercitationem laborum, facere
+        perferendis aliquid id iusto iure quas odio cumque earum, ut accusamus
+        enim aliquam.
+      </p>
+    </section>
+  );
+}
+
+export default CallToAction;
