c5eb8ff525ff4f545625d7a4b04399742f78e878
Mert Guler <gulermert94@gmail.com>
2023-03-01 20:29:31 -0500

    .env possible solution

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
-const { parsed: myEnv } = require("dotenv").config();
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv ?? ""));
-    return config;
-  },
-};
