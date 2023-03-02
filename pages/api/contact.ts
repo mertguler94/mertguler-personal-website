a0ad91d8681fb56a305e53e5559aa2ecb7229cec
Mert Guler <gulermert94@gmail.com>
2023-03-01 20:45:48 -0500

    try catch for email sending

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,28 +5,34 @@ const user = process.env.GMAIL_USER;
 const pass = process.env.GMAIL_PASSWORD;
 
 export default function contact(req: NextApiRequest, res: NextApiResponse) {
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user,
-      pass,
-    },
-    secure: true,
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user,
+        pass,
+      },
+      secure: true,
+    });
 
-  const mailData = {
-    from: req.body.email,
-    to: user,
-    subject: `${req.body.subject} | ${req.body.name}`,
-    text: req.body.message,
-    html: `<h3>${req.body.subject}</h3><p>${req.body.message}</p><p>${req.body.email}</p><p>${req.body.name}</p>`,
-  };
+    const mailData = {
+      from: req.body.email,
+      to: user,
+      subject: `${req.body.subject} | ${req.body.name}`,
+      text: req.body.message,
+      html: `<h3>${req.body.subject}</h3><p>${req.body.message}</p><p>${req.body.email}</p><p>${req.body.name}</p>`,
+    };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.log(err);
-    else console.log(info);
-  });
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
 
-  res.status(200).json({ message: "Successful" });
+      res.status(200).json({ message: "Successful" });
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(400).json({ message: "email could not be sent." });
+  }
 }
